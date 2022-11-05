2f7345c03e9b9ab65f874c994165864a1da0b725
kristbaum <max@kristenonline.de>
2022-11-05 01:52:33 +0100

    downgrade user

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-python3 manage.py runserver 0.0.0.0:8000
+#python3 manage.py runserver 0.0.0.0:8000
+#echo Running here:
+#pwd
+#ls
+#sleep 1000
+
+uwsgi \
+--socket :8000 \
+--wsgi-file core/wsgi.py \
+--master \
+--processes 1 \
+--threads 1 
+
+#--chdir /api/ \
+
+#
+#--stats :8001
+
+#api/dacit_api/dacit_api/wsgi.py
+#api/dacit_api/manage.py

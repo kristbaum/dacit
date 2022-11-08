7224341d0d9def76de0e0a2740e73dbbb84ce8bc
kristbaum <max@kristenonline.de>
2022-11-08 02:59:16 +0100

    initial min game

--- dacit/lib/minimal_pairs_page.dart
+++ dacit/lib/minimal_pairs_page.dart
@@ -1,8 +1,7 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:just_audio/just_audio.dart';
 
 class MinimalPairs extends StatefulWidget {
   const MinimalPairs({Key? key}) : super(key: key);
@@ -12,6 +11,21 @@ class MinimalPairs extends StatefulWidget {
 }
 
 class MinimalPairsState extends State<MinimalPairs> {
+  late Future<Minpair> _minpair;
+  late AudioPlayer player;
+  @override
+  void initState() {
+    super.initState();
+    _minpair = getMinPair();
+    player = AudioPlayer();
+  }
+
+  @override
+  void dispose() {
+    player.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,15 +34,68 @@ class MinimalPairsState extends State<MinimalPairs> {
       ),
       body: Center(
           child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          IconButton(
-            icon: const Icon(Icons.get_app),
-            tooltip: 'Test connection',
-            onPressed: () {
-              getMinPair();
-            },
-          ),
-          const Text('This is an app developed for a master thesis etc')
+          const Text("Sind diese beiden Worte gleich?"),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FutureBuilder<Minpair>(
+                  future: _minpair,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return ElevatedButton(
+                          onPressed: () async {
+                            await player
+                                .setUrl(snapshot.data!.firstAudio.toString());
+                            player.play();
+                          },
+                          child: const Text('A'));
+                    } else if (snapshot.hasError) {
+                      return Text('${snapshot.error}');
+                    }
+                    return const CircularProgressIndicator();
+                  },
+                ),
+                Column(
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.done),
+                      tooltip: 'Test connection',
+                      onPressed: () {
+                        getMinPair();
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.close),
+                      tooltip: 'Test connection',
+                      onPressed: () {
+                        getMinPair();
+                      },
+                    ),
+                  ],
+                ),
+                FutureBuilder<Minpair>(
+                  future: _minpair,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return ElevatedButton(
+                          onPressed: () async {
+                            await player
+                                .setUrl(snapshot.data!.secondAudio.toString());
+                            player.play();
+                          },
+                          child: const Text('B'));
+                    } else if (snapshot.hasError) {
+                      return Text('${snapshot.error}');
+                    }
+                    return const CircularProgressIndicator();
+                  },
+                ),
+              ],
+            ),
+          )
         ],
       )),
     );
@@ -39,19 +106,10 @@ Future<Minpair> getMinPair() async {
   var response =
       await http.get(Uri.parse("http://localhost:8000/minpair?category=K_T"));
   if (response.statusCode == 200) {
-    var responseData = json.decode(response.body);
-    log(responseData.toString());
-    Minpair minPair = Minpair(
-      id: responseData["minpair"],
-      firstStimulus: responseData["first_stimulus"],
-      firstAudio: Uri.parse(responseData["first_audio"]),
-      secondText: responseData["second_text"],
-      secondAudio: Uri.parse(responseData["second_audio"]),
-    );
-    return minPair;
+    return Minpair.fromJson(json.decode(response.body));
+//    audioSource = ap.AudioSource.uri(minPair.firstAudio);
   } else {
-    log("Server could not be reached");
-    throw Error();
+    throw Exception("Failed to load Minpair!");
   }
 }
 
@@ -59,13 +117,23 @@ class Minpair {
   final int id;
   final String firstStimulus;
   final Uri firstAudio;
-  final String secondText;
+  final String secondStimulus;
   final Uri secondAudio;
 
   const Minpair(
       {required this.id,
       required this.firstStimulus,
       required this.firstAudio,
-      required this.secondText,
+      required this.secondStimulus,
       required this.secondAudio});
+
+  factory Minpair.fromJson(Map<String, dynamic> json) {
+    return Minpair(
+      id: json['minpair'],
+      firstStimulus: json["first_stimulus"],
+      firstAudio: Uri.parse(json["first_audio"]),
+      secondStimulus: json["second_stimulus"],
+      secondAudio: Uri.parse(json["second_audio"]),
+    );
+  }
 }
